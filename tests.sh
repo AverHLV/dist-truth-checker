88cf535cc32d4419c83e0978444a78260f963c03
AverHLV <moc9.cs4ever@gmail.com>
2019-05-29 01:28:52 +0300

    codecov command fix [1]

--- tests.sh
+++ tests.sh
@@ -6,4 +6,4 @@ coverage run -a topic_modeling/manage.py test api.tests
 coverage run -a seo_analysis/manage.py test api.tests
 coverage report
 
-sudo pip install --user codecov && codecov -t $CODECOV_TOKEN
+pip install codecov && codecov -t $CODECOV_TOKEN
