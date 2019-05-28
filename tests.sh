b66249fac0b307498d13e6c99ba2963d78483543
AverHLV <moc9.cs4ever@gmail.com>
2019-05-28 18:37:44 +0300

    gitlab yaml changed [2]

--- tests.sh
+++ tests.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+echo $GW_SECRET | base64 -d > /builds/AverHLV/dist-truth-checker/gateway/config/secret.json
+echo $SEO_SECRET | base64 -d > /builds/AverHLV/dist-truth-checker/seo_analysis/config/secret.json
+echo $TM_SECRET | base64 -d > /builds/AverHLV/dist-truth-checker/topic_modeling/config/secret.json
+
 coverage run -a gateway/manage.py test
 coverage run -a topic_modeling/manage.py test
 coverage run -a seo_analysis/manage.py test
