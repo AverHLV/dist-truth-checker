e3b5218447acc64ed27be90dcd5e17d0d4d0439a
AverHLV <moc9.cs4ever@gmail.com>
2019-05-29 01:13:06 +0300

    Run tests commands fix

--- tests.sh
+++ tests.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
 
-python gateway/manage.py makemigrations texts texts_admin
-python gateway/manage.py migrate
-python gateway/manage.py test
-
-python topic_modeling/manage.py makemigrations api
-python topic_modeling/manage.py migrate
-python topic_modeling/manage.py test
-
-python seo_analysis/manage.py makemigrations api
-python seo_analysis/manage.py migrate
-python seo_analysis/manage.py test
-
-coverage run -a gateway/manage.py test
-coverage run -a topic_modeling/manage.py test
-coverage run -a seo_analysis/manage.py test
+coverage run -a gateway/manage.py test texts.tests
+coverage run -a gateway/manage.py test texts_admin.tests
+coverage run -a topic_modeling/manage.py test api.tests
+coverage run -a seo_analysis/manage.py test api.tests
 coverage report
+
+pip install --user codecov && codecov -t d86457a1-1b31-47a2-b10a-1505f6b10a19
