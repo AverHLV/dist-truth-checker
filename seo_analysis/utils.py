1d0e60d09ecf715f0e6350dc54563c5d07c7e0ca
AverHLV <moc9.cs4ever@gmail.com>
2019-05-29 12:30:39 +0300

    secret load changed for local settings

--- seo_analysis/utils.py
+++ seo_analysis/utils.py
@@ -20,7 +20,7 @@ def get_secret(filename):
     except IOError:
         # local and ci settings
 
-        return {}
+        return {'service_tokens': ['59c9c527b426e68362fad567fa587a63e7134ac3a7d1c1f877e23eadef9a8a3d']}
 
 
 secret_dict = get_secret(constants.secret_filename)
